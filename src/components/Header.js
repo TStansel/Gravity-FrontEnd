464e2ba19271d764b088806097d22fb31961ec0a
justin-hill426 <jrhill426@gmail.com>
2022-04-08 02:27:42 -0600

    Added initial animations

--- src/components/Header.js
+++ src/components/Header.js
@@ -274,6 +274,7 @@ const Header = (props) => {
         <AppBar
           sx={(theme) => ({
             zIndex: theme.zIndex.modal + 1,
+            background: "conic-gradient(from -26.29deg at 64.69% 113.89%, #86C0F6 -31.88deg, #A259FF 11.25deg, #E086F6 166.87deg, #86C0F6 328.12deg, #A259FF 371.25deg)"
           })}
         >
           <Toolbar disableGutters>
